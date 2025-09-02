1bdd5e86f05e91cddf022405ad7f611627b228da
Felipe Trzaskowski <felipe.trzaskowski@maistodos.com.br>
2025-09-02 09:55:40 -0300

    - Refatora install_driver.ps1 e melhora tratamento de erros - Não desinstala driver do fabricante se não encontrar impressora conhecida

--- driver_installer/install_driver.ps1
+++ driver_installer/install_driver.ps1
@@ -1,4 +1,8 @@
+Set-StrictMode -Version Latest
 $PSDefaultParameterValues['*:Encoding'] = 'utf8'
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
 
 $manufacturer = 'MaisTODOS'
 $name = 'MaisTODOS POS Series Printer'
@@ -10,47 +14,89 @@ $device_ids = @(
     @{ VID = '28e9'; PID = '0289' }
 )
 
-try {
-    $driver_package = Get-Package "POS Series Printer Driver*" -ErrorAction Stop
-} catch {
-    $driver_package = $null
-}
-if ($driver_package) {
-    Write-Output 'Driver do fabricante encontrado, desinstalando...'
-    $uninstall_string = $driver_package.Meta.Attributes['QuietUninstallString']
-    Start-Process -FilePath cmd.exe -ArgumentList '/c',$uninstall_string -Wait
-}
 
-$found = $false
+function Find-Existing-Devices {
+    $found = @()
 
-foreach ($device_id in $device_ids) {
-	$devices = Get-PnpDevice | Where-Object {
-		$_.InstanceId -match "VID_$($device_id.VID)&PID_$($device_id.PID)" 
-	}
-
-    if ($devices) {
-        $found = $true
-        Write-Output 'Substituindo driver para o dispositivo:'
-        Write-Output "  VID=$($device_id.VID) PID=$($device_id.PID)"
-
-        foreach ($device in $devices) {
-            pnputil /remove-device $device.InstanceId /force
+    foreach ($device_id in $device_ids) {
+        $devices = Get-PnpDevice | Where-Object {
+            $_.InstanceId -match "VID_$($device_id.VID)&PID_$($device_id.PID)" 
         }
 
-        .\wdi-simple.exe --stealth-cert -t 0 `
-            -n $name `
-            -m $manufacturer `
-            -v 0x$($device_id.VID) `
-            -p 0x$($device_id.PID)
+        if ($devices) {
+            $found += @{
+                VID = $device_id.VID;
+                PID = $device_id.PID;
+                DEVICES = $devices
+            }
+        }
+    }
 
-        pnputil /scan-devices
+    return $found
+}
+
+
+function Uninstall-Manufacturer-Driver {
+    try {
+        $driver_package = Get-Package "POS Series Printer Driver*"
+    } catch {
+        $driver_package = $null
+    }
+
+    if ($driver_package) {
+        Write-Host 'Driver do fabricante encontrado, desinstalando...'
+        $uninstall_string = $driver_package.Meta.Attributes['QuietUninstallString']
+        Start-Process -FilePath cmd.exe -ArgumentList '/c',$uninstall_string -Wait
     }
 }
 
-if ($found) {
-    Write-Output 'Drivers instalados com sucesso!'
-} else {
-    Write-Output 'Nenhuma impressora conhecida foi encontrada.'
+
+function Replace-Device-Driver {
+    param (
+        $found_device
+    )
+
+    $device_vid = $found_device.VID
+    $device_pid = $found_device.PID
+    $devices = $found_device.DEVICES
+
+    Write-Host 'Substituindo driver para o dispositivo:'
+    Write-Host "  VID=$device_vid PID=$device_pid"
+
+    foreach ($device in $devices) {
+        pnputil /remove-device $device.InstanceId /force | Out-Null
+    }
+
+    .\wdi-simple.exe --stealth-cert -t 0 `
+        -n $name `
+        -m $manufacturer `
+        -v 0x$device_vid `
+        -p 0x$device_pid
 }
 
-Read-Host -Prompt 'Pressione Enter para fechar' | Out-Null
+
+function Main {
+    $found_devices = Find-Existing-Devices
+
+    if ($found_devices) {
+        Uninstall-Manufacturer-Driver
+
+        pnputil /scan-devices | Out-Null
+
+        foreach ($found_device in $found_devices) {
+            Replace-Device-Driver $found_device
+        }
+
+        pnputil /scan-devices | Out-Null
+
+        Write-Host 'Drivers instalados com sucesso!'
+    } else {
+        Write-Host 'Nenhuma impressora conhecida foi encontrada.'
+    }
+}
+
+try {
+    Main
+} finally {
+    Read-Host -Prompt 'Pressione Enter para fechar' | Out-Null
+}
