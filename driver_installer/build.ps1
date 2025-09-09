6a3a6b0c48901d9f4c0436c00a01e0e56ab66bfa
Felipe Trzaskowski <felipe.trzaskowski@maistodos.com.br>
2025-09-09 12:10:13 -0300

    Adiciona instalação de drivers de maquininha de cartão

--- driver_installer/build.ps1
+++ driver_installer/build.ps1
@@ -1,8 +1,12 @@
+$prefix = "$env:TEMP\maistodos_pos_installer"
+
 $embedded_files = @{
-    'wdi-simple.exe' = '.\wdi-simple.exe';
+    "$prefix\wdi-simple.exe" = '.\installers\wdi-simple.exe';
+    "$prefix\gertec-installer.exe" = '.\installers\gertec-installer.exe';
+    "$prefix\ingenico-installer.exe" = '.\installers\ingenico-installer.exe'
 }
 
-ps2exe '.\install_driver.ps1' '.\install_driver.exe' `
+ps2exe '.\setup.ps1' '.\setup.exe' `
     -verbose `
     -requireAdmin `
     -embedFiles $embedded_files
